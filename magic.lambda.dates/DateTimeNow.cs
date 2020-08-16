fce9a72c16fa9aaf6b5b2398a05058cdf2b5c09d
Thomas Hansen <thomas@gaiasoul.com>
2020-08-16 12:41:32 +0300

    Update DateTimeNow.cs

--- magic.lambda.dates/DateTimeNow.cs
+++ magic.lambda.dates/DateTimeNow.cs
@@ -27,9 +27,9 @@ namespace magic.lambda.dates
         {
             var format = input.Children.FirstOrDefault(x => x.Name == "format")?.GetEx<string>();
             if (format == null)
-                input.Value = DateTime.Now;
+                input.Value = DateTime.Now.ToUniversalTime();
             else
-                input.Value = DateTime.Now.ToString(format, CultureInfo.InvariantCulture);
+                input.Value = DateTime.Now.ToUniversalTime().ToString(format, CultureInfo.InvariantCulture);
         }
     }
 }
