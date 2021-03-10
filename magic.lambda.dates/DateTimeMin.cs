b912e430fcb742f80169edb6ea7c583aee1b6e43
Thomas Hansen <thomas@gaiasoul.com>
2021-03-10 10:16:35 +0200

    Adding [date.min] slot

---
+++ magic.lambda.dates/DateTimeMin.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Magic, Copyright(c) Thomas Hansen 2019 - 2020, thomas@servergardens.com, all rights reserved.
+ * See the enclosed LICENSE file for details.
+ */
+
+using System;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.dates
+{
+    /// <summary>
+    /// [date.min] slot, returning minimum value for DateTime type.
+    /// </summary>
+    [Slot(Name = "date.min")]
+    public class DateTimeMin : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = DateTime.MinValue;
+        }
+    }
+}
