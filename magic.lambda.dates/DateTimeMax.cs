ae7cec1d65680f06241faee9de1636d76c8873f8
Thomas Hansen <thomas@gaiasoul.com>
2021-12-26 08:48:28 +0200

    Adding [date.max]

---
+++ magic.lambda.dates/DateTimeMax.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Magic Cloud, copyright Aista, Ltd. See the attached LICENSE file for details.
+ */
+
+using System;
+using magic.node;
+using magic.signals.contracts;
+
+namespace magic.lambda.dates
+{
+    /// <summary>
+    /// [date.max] slot, returning maximum value for DateTime type.
+    /// </summary>
+    [Slot(Name = "date.max")]
+    public class DateTimeMax : ISlot
+    {
+        /// <summary>
+        /// Implementation of signal
+        /// </summary>
+        /// <param name="signaler">Signaler used to signal</param>
+        /// <param name="input">Parameters passed from signaler</param>
+        public void Signal(ISignaler signaler, Node input)
+        {
+            input.Value = DateTime.MaxValue;
+        }
+    }
+}
